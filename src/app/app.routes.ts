530db5a4cd9f899288e148fb535ad591bd773102
Cristian Espinoza <cespinoza@factumx.net>
2017-10-12 00:28:27 -0400

    Corrección para la redirección al home.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -12,7 +12,7 @@ const app_routes: Routes = [
   {path: 'about', component: AboutComponent},
   {path: 'producto/:id', component: ProductoComponent},
   {path: 'buscar/:termino', component: SearchComponent},
-  {path: '**', pathMatch: 'full', redirectTo: ''}
+  {path: '**', pathMatch: 'full', redirectTo: 'home'}
 ];
 
 export const app_routing = RouterModule.forRoot(app_routes, {useHash: true});
