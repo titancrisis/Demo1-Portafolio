d47fee8e0f9a35acf64575ca16c949933973d71e
Cristian Espinoza <cespinoza@factumx.net>
2017-10-11 23:31:45 -0400

    Se completo los cambios para la busqueda.

--- src/app/services/productos/productos.service.ts
+++ src/app/services/productos/productos.service.ts
@@ -1,10 +1,12 @@
 import {Injectable} from '@angular/core';
 import {Http} from '@angular/http';
+import {reject} from "q";
 
 @Injectable()
 export class ProductosService {
 
   productos: any[] = [];
+  productos_filtrado: any[] = [];
   cargando_productos: boolean = true;
 
   constructor(private http: Http) {
@@ -13,19 +15,55 @@ export class ProductosService {
 
   public cargar_productos() {
     this.cargando_productos = true;
-    if (this.productos.length === 0) {
-      this.http.get('https://paginaweb-angular.firebaseio.com/productos_idx.json')
-        .subscribe(res => {
-          // console.log(res.json());
-          // setTimeout(() => {
-          this.cargando_productos = false;
-          this.productos = res.json();
-          // }, 1500);
-        });
-    }
+
+    let promesa = new Promise((resolve, reject) => {
+      if (this.productos.length === 0) {
+        this.http.get('https://paginaweb-angular.firebaseio.com/productos_idx.json')
+          .subscribe(res => {
+            // console.log(res.json());
+            // setTimeout(() => {
+            this.cargando_productos = false;
+            this.productos = res.json();
+
+            resolve();
+            // }, 1500);
+          });
+      }
+    });
+
+    return promesa;
   }
 
   public cargar_producto(cod: string) {
     return this.http.get(`https://paginaweb-angular.firebaseio.com/productos/${cod}.json`);
+  }
+
+  public buscar_producto(termino: string) {
+    console.log('Buscando producto');
+    console.log(this.productos.length);
+
+    if (this.productos.length === 0) {
+      this.cargar_productos().then(() => {
+// termino la carga.
+        this.filtrar_productos(termino);
+      });
+    } else {
+      this.filtrar_productos(termino);
+    }
+  }
+
+  private filtrar_productos(termino: string) {
+
+    this.productos_filtrado = [];
+    termino = termino.toLowerCase();
+
+    this.productos.forEach(prod => {
+
+      if (prod.categoria.indexOf(termino) >= 0 || prod.titulo.toLowerCase().indexOf(termino) >= 0) {
+        this.productos_filtrado.push(prod);
+      }
+
+      console.log(prod);
+    });
   }
 }
